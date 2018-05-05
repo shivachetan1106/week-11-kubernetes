6f3a8c9372a6b9a99d222f2875f0626bcbe5cd9a
root <root@localhost.localdomain>
2018-05-04 21:38:51 -0600

    kubernetes hw

---
+++ shgo2887/Week-11-Kubernetes/README.md
@@ -0,0 +1,5 @@
+This is the submission for the kubernetes assignment:
+
+Please run the web and db yaml 
+
+Check the services that are running
---
+++ shgo2887/Week-11-Kubernetes/web/index.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "172.19.0.1";
+$username = 'root'; 
+$password = 'secret';
+$dbname = 'database';
+$port=3306;
+$conn = mysqli_connect($servername, $username, $password, $dbname,$port);
+$sql = "SELECT recipe_id,recipe_name FROM database.recipes;";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["recipe_id"]. " - Name: " . $row["recipe_name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+//<?php
+//phpinfo();
+//?>
